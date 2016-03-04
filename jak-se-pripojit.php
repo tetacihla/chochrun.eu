b4061de7e0d283292d558c4ca4600ae05bf7add7
Lukas Chochrun <tetacihla@github.com>
2016-03-05 00:53:12 +0100

    Pridal jsem neco

--- jak-se-pripojit.php
+++ jak-se-pripojit.php
@@ -9,7 +9,7 @@
                  
                 <div id="post_wrapper">
                 
-                    fdfdfdfd
+                    test
                     <!-- Body Start -->
                     <div id="body">
                     	<h1>Jak se připojit na naše služby</h1>
